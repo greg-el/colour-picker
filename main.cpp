83f2f45b5b149ca3ccf304e39a5537df5fe1b6c4
Greg Leonard <gr3gl@hotmail.com>
2019-12-17 23:20:08 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+struct ClickCoordinates {
+    int x;
+    int y;
+};
+
+unsigned long rgbToHex(int r, int g, int b) {
+        return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
+
+ClickCoordinates coordinates (){
+    ClickCoordinates click;
+    int x=-1,y=-1;
+    XEvent event;
+    int button;
+    Display *display = XOpenDisplay(NULL);
+
+    if (display == NULL) {
+        fprintf(stderr, "Cannot connect to X server!\n");
+        exit (EXIT_FAILURE);
+    }
+
+    Window root= XDefaultRootWindow(display);
+    XGrabPointer(display, root, False, ButtonPressMask, GrabModeAsync,
+         GrabModeAsync, None, None, CurrentTime);
+
+    XSelectInput(display, root, ButtonReleaseMask) ;
+
+    while(1){
+        XNextEvent(display,&event);
+        switch(event.type){
+        case ButtonPress:
+            switch(event.xbutton.button){
+                case Button1:
+                    x=event.xbutton.x;
+                    y=event.xbutton.y;
+                    button=Button1;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+        }
+        if(x>=0 && y>=0)break;
+    }
+    
+    XCloseDisplay(display);
+    click.x = x;
+    click.y = y;
+    return click;
+}
+
+int main(int, char**) {
+    ClickCoordinates click;
+    XColor c;
+    Display *d = XOpenDisplay((char *) NULL);
+
+    click = coordinates();
+    int x=click.x;  // Pixel x 
+    int y=click.y;  // Pixel y
+
+    XImage *image;
+    image = XGetImage (d, XRootWindow (d, XDefaultScreen (d)), x, y, 1, 1, AllPlanes, XYPixmap);
+    c.pixel = XGetPixel (image, 0, 0);
+    XFree (image);
+    XQueryColor (d, XDefaultColormap(d, XDefaultScreen (d)), &c);
+    unsigned long hex = rgbToHex(c.red/256, c.green/256, c.blue/256);
+    std::cout << "#" << std::hex << hex;
+    std::cout << "\n";
+    return 0;
+}
+
+
+
